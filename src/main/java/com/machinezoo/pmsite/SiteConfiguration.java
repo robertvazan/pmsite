efac6e47dcab2296d751da6800b2a9f6baab5d1c
Robert Vazan <robvcode@gmail.com>
2019-10-12 11:51:05 +0000

    Generate sitemap

--- src/main/java/com/machinezoo/pmsite/SiteConfiguration.java
+++ src/main/java/com/machinezoo/pmsite/SiteConfiguration.java
@@ -2,7 +2,12 @@
 package com.machinezoo.pmsite;
 
 import java.net.*;
+import java.nio.*;
+import java.nio.charset.*;
+import java.util.function.*;
+import com.machinezoo.hookless.servlets.*;
 import com.machinezoo.pushmode.*;
+import cz.jiripinkas.jsitemapgenerator.generator.*;
 
 public abstract class SiteConfiguration {
 	public abstract URI uri();
@@ -18,10 +23,30 @@ public abstract class SiteConfiguration {
 		String buster = hash != null ? "?v=" + hash : SiteReload.buster();
 		return path + buster;
 	}
+	public SitemapGenerator sitemap() {
+		return SitemapGenerator.of(uri().toString());
+	}
 	public SiteConfiguration() {
 		mappings
 			.map("/pushmode/poller", new PollServlet())
 			.map("/pushmode/submit", new SubmitServlet())
-			.map("/pushmode/script", new PushScriptServlet());
+			.map("/pushmode/script", new PushScriptServlet())
+			.map("/sitemap.xml", new SitemapServlet(this::sitemap));
+	}
+	@SuppressWarnings("serial") private static class SitemapServlet extends ReactiveServlet {
+		final Supplier<SitemapGenerator> supplier;
+		SitemapServlet(Supplier<SitemapGenerator> supplier) {
+			this.supplier = supplier;
+		}
+		@Override public ReactiveServletResponse doGet(ReactiveServletRequest request) {
+			SitemapGenerator sitemap = supplier.get();
+			byte[] data = sitemap.toString().getBytes(StandardCharsets.UTF_8);
+			ReactiveServletResponse response = new ReactiveServletResponse();
+			response.headers().put("Content-Type", "text/xml; charset=utf-8");
+			response.headers().put("Content-Length", Integer.toString(data.length));
+			response.headers().put("Cache-Control", "public, max-age=86400");
+			response.data(ByteBuffer.wrap(data));
+			return response;
+		}
 	}
 }
