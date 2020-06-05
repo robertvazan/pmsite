d4afebf449a996e19f83a56e4d90298524591fb5
Robert Važan <robert.vazan@protonmail.com>
2020-06-05 20:07:16 +0000

    Created dummy test to unblock builds

---
+++ src/test/java/com/machinezoo/pmsite/DummyTest.java
@@ -0,0 +1,10 @@
+// Part of PMSite: https://pmsite.machinezoo.com
+package com.machinezoo.pmsite;
+
+import org.junit.jupiter.api.*;
+
+public class DummyTest {
+	@Test public void dummy() {
+		SiteFiles.config();
+	}
+}
