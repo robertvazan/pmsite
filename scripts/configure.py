40c6975cc3970b93545babb9fdeabe300b668393
Robert Važan <robert.vazan@tutanota.com>
2023-04-08 12:55:01 +0000

    Updated project configuration

--- scripts/configure.py
+++ scripts/configure.py
@@ -1,47 +1,43 @@
 # This script generates and updates project configuration files.
 
-# We are assuming that project-config is available in sibling directory.
-# Checkout from https://github.com/robertvazan/project-config
-import pathlib
-project_directory = lambda: pathlib.Path(__file__).parent.parent
-config_directory = lambda: project_directory().parent/'project-config'
-exec((config_directory()/'src'/'java.py').read_text())
+# Run this script with rvscaffold in PYTHONPATH
+import rvscaffold as scaffold
 
-project_script_path = __file__
-repository_name = lambda: 'pmsite'
-pretty_name = lambda: 'PMSite'
-pom_description = lambda: "Simplistic application framework built on top of PushMode HTML streaming library."
-inception_year = lambda: 2017
-jdk_version = lambda: 17
-has_javadoc = lambda: False
-stagean_annotations = lambda: True
-project_status = lambda: experimental_status()
+class Project(scaffold.Java):
+    def script_path_text(self): return __file__
+    def repository_name(self): return 'pmsite'
+    def pretty_name(self): return 'PMSite'
+    def pom_description(self): return "Simplistic application framework built on top of PushMode HTML streaming library."
+    def inception_year(self): return 2017
+    def jdk_version(self): return 17
+    def has_javadoc(self): return False
+    def stagean_annotations(self): return True
+    
+    def dependencies(self):
+        yield from super().dependencies()
+        yield self.use_hookless()
+        yield self.use_pushmode()
+        # Insist on older SLF4J. Otherwise jetty would pull in 2.0 version.
+        yield self.use_slf4j()
+        yield self.use_noexception_slf4j()
+        yield self.use_streamex()
+        yield self.use_commons_lang()
+        yield self.use_commons_math()
+        yield self.use_guava()
+        # Used just to load MIME types from resources.
+        yield self.use_gson()
+        yield self.use('org.eclipse.jetty.websocket:websocket-servlet:11.0.7')
+        yield self.use('net.mikehardy:google-analytics-java:2.0.11')
+        yield self.use('cz.jiripinkas:jsitemapgenerator:4.5')
+        yield self.use_junit()
+        yield self.use_slf4j_test()
+    
+    def javadoc_links(self):
+        yield 'https://stagean.machinezoo.com/javadoc/'
+        yield 'https://noexception.machinezoo.com/javadoc/'
+        yield 'https://hookless.machinezoo.com/javadocs/core/'
+        yield 'https://hookless.machinezoo.com/javadocs/servlets/'
+        yield 'https://pushmode.machinezoo.com/javadoc/'
+        # Jsitemapgenerator not linked, because automatic modules are not supported by javadoc.
 
-def dependencies():
-    use_hookless()
-    use_pushmode()
-    # Insist on older SLF4J. Otherwise jetty would pull in 2.0 version.
-    use_slf4j()
-    use_noexception_slf4j()
-    use_streamex()
-    use_commons_lang()
-    use_commons_math()
-    use_guava()
-    # Used just to load MIME types from resources.
-    use_gson()
-    use('org.eclipse.jetty.websocket:websocket-servlet:11.0.7')
-    use('net.mikehardy:google-analytics-java:2.0.11')
-    use('cz.jiripinkas:jsitemapgenerator:4.5')
-    use_junit()
-    use_slf4j_test()
-
-javadoc_links = lambda: [
-    'https://stagean.machinezoo.com/javadoc/',
-    'https://noexception.machinezoo.com/javadoc/',
-    'https://hookless.machinezoo.com/javadocs/core/',
-    'https://hookless.machinezoo.com/javadocs/servlets/',
-    'https://pushmode.machinezoo.com/javadoc/'
-    # Jsitemapgenerator not linked, because automatic modules are not supported by javadoc.
-]
-
-generate()
+Project().generate()
